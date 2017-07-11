3ef21fa75a5605cf8642dfb286e7f0945d24cae4
Enoc Pineda <enoc_pineda@hotmail.com>
2017-07-10 21:51:29 -0500

    Curso Ruby

---
+++ Coversion.rb
@@ -0,0 +1,9 @@
+#Trasnformar yenes a pesos
+
+Pesos = 3000
+def yenes_to_currency(yennes)
+  tipo_Cambio =  6.38
+  return Pesos * tipo_Cambio
+end
+
+puts yenes_to_currency(200)
---
+++ HolaMundo.rb
@@ -0,0 +1,11 @@
+puts "Hola mundo"
+puts "Hola"
+puts "Camara"
+
+def hola
+  nombre_de_curso = "Curso Ruby"
+  nombre = "Enoc"
+puts "Hola #{nombre} Bienvenido a #{nombre_de_curso}"
+end
+
+hola
